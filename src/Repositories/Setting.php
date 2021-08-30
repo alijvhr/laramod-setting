648bf5cf25f593f243e3ec1607f78eed702d8409
PouriyaK7 <pk74ever@gmail.com>
2021-08-31 02:29:34 +0430

    - Fixed expiration bug in Setting - Changed TTL to 1 minute from 15 minutes

--- src/Repositories/Setting.php
+++ src/Repositories/Setting.php
@@ -7,18 +7,19 @@ use Illuminate\Support\Facades\Redis;
 
 class Setting
 {
-    private static int $expirationMinutes = 15;
+    private static int $expirationMinutes = 1;
 
     public static function set(string $key, mixed $value = null): void
     {
-        if (Cache::has($key) || $key == 'all_keys')
+        if (Cache::has($key))
             return;
 
         if (is_array($value))
             $value = json_encode($value);
 
         Cache::forever($key, $value);
-        Redis::set($key, $value, now()->diffInSeconds(now()->addMinutes(self::$expirationMinutes)));
+        Redis::set($key, $value);
+        Redis::expire($key, now()->diffInSeconds(now()->addMinutes(self::$expirationMinutes)));
     }
 
     public static function remove(string $key): void
@@ -30,7 +31,10 @@ class Setting
     public static function get(string $key): mixed
     {
         if (!Cache::offsetExists($key)) return null;
-        if (!Redis::exists($key)) Redis::set($key, Cache::get($key), now()->diffInSeconds(now()->addMinutes(self::$expirationMinutes)));
+        if (!Redis::exists($key)) {
+            Redis::set($key, Cache::get($key));
+            Redis::expire($key, now()->diffInSeconds(now()->addMinutes(self::$expirationMinutes)));
+        }
 
         return Redis::get($key);
     }
