6274ace0913b562a875b873ceccba35d1575c8c7
PouriyaK7 <pk74ever@gmail.com>
2021-09-23 12:56:48 +0330

    - Added permission middlewares to routes

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Sparrow\Setting\Http\Controllers\SettingController;
 
 Route::group(['middleware' => ['web']], function () {
-    Route::group(['prefix' => 'admin/settings'], function () {
+    Route::group(['prefix' => 'admin/settings', 'middleware' => ['role:admin']], function () {
         Route::get('/', [SettingController::class, 'index'])->name('site.admin.settings.index');
     });
-    Route::post('api/admin/settings/update', [SettingController::class, 'set'])->name('api.admin.settings.update');
+    Route::post('api/admin/settings/update', [SettingController::class, 'set'])->middleware('role:admin')->name('api.admin.settings.update');
 });
