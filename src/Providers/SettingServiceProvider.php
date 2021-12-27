0b2042916a804fd14adc5aae507aed8bf79b0472
POuriya Khazaei <kpouriya6@gmail.com>
2021-12-27 16:54:32 +0330

    Added settings swoole table

--- src/Providers/SettingServiceProvider.php
+++ src/Providers/SettingServiceProvider.php
@@ -13,9 +13,17 @@ class SettingServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sparrow-setting');
         $this->loadMigrationsFrom([__DIR__ . '/../database/migrations']);
         $this->loadRoutesFrom(__DIR__ . '/../routes/routes.php');
-        $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/sparrow/support')
-        ]);
+        $this->publishes([__DIR__ . '/../resources/views' => resource_path('views/sparrow/support')]);
         $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'setting');
+        $tables = config('swoole_http.tables');
+        $tables['settings'] = [
+            'size' => 4096,
+            'columns' => [
+                ['name' => 'counter', 'type' => Table::TYPE_INT, 'size' => 11],
+                ['name' => 'value', 'type' => Table::TYPE_STRING, 'size' => 1024],
+                ['name' => 'type', 'type' => Table::TYPE_STRING, 'size' => 1024]
+            ]
+        ];
+        config(['swoole_http.tables' => $tables]);
     }
 }
