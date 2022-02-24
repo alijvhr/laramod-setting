378dbf001f4f9e66ff5ba5c8de39b2c7388dcd2d
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2022-02-24 15:31:32 +0330

    Single drivered drivers test

--- src/Tests/SettingDriversTest.php
+++ src/Tests/SettingDriversTest.php
@@ -7,50 +7,36 @@ use Tests\TestCase;
 
 class SettingDriversTest extends TestCase
 {
-    private array $drivers = [
-        'setting.redis',
-        'setting.swoole',
-        'setting.db',
-    ];
-
     public function test_can_set_key()
     {
-        foreach ($this->drivers as $driver) {
-            $driver = Setting::getDriver($driver);
-            $driver->set('test', 'test');
-            $this->assertEquals('test', $driver->get('test'));
-        }
+        $driver = Setting::getDriver();
+        $driver->set('test', 'test');
+        $this->assertEquals('test', $driver->get('test'));
     }
 
     public function test_can_remove_key()
     {
-        foreach ($this->drivers as $driver) {
-            $driver = Setting::getDriver($driver);
-            $this->assertTrue($driver->exists('test'));
-            $driver->remove('test');
-            $this->assertFalse($driver->exists('test'));
-        }
+        $driver = Setting::getDriver();
+        $this->assertTrue($driver->exists('test'));
+        $driver->remove('test');
+        $this->assertFalse($driver->exists('test'));
     }
 
     public function test_can_increment_key()
     {
-        foreach ($this->drivers as $driver) {
-            $driver = Setting::getDriver($driver);
-            $driver->set('test', 0);
-            $driver->incr('test');
-            $this->assertEquals(1, $driver->get('test'));
-            $driver->remove('test');
-        }
+        $driver = Setting::getDriver();
+        $driver->set('test', 0);
+        $driver->incr('test');
+        $this->assertEquals(1, $driver->get('test'));
+        $driver->remove('test');
     }
 
     public function test_can_decrement_key()
     {
-        foreach ($this->drivers as $driver) {
-            $driver = Setting::getDriver($driver);
-            $driver->set('test', 1);
-            $driver->decr('test');
-            $this->assertEquals(0, $driver->get('test'));
-            $driver->remove('test');
-        }
+        $driver = Setting::getDriver();
+        $driver->set('test', 1);
+        $driver->decr('test');
+        $this->assertEquals(0, $driver->get('test'));
+        $driver->remove('test');
     }
 }
